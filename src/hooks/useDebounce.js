a2205dfd1f8175fc18768d4f207cbc9ee76d10da
itzmeimran <ikik790@gmail.com>
2025-12-23 16:48:12 +0530

    first commit

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,8 @@
+export function useDebounce(value, delay = 500) {
+  const [debounced, setDebounced] = useState(value);
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+  return debounced;
+}
